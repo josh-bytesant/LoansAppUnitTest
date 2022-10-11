d2039a1ba209b50b4ae28cc86c907d3de4516ae8
Joshua Uwagwu <chuksjosh20@gmail.com>
2022-10-11 08:14:36 +0100

    first commit

---
+++ Loans.Tests/LoanTermShould.cs
@@ -0,0 +1,21 @@
+﻿using Loans.Domain.Applications;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loans.Tests
+{
+    [TestFixture]
+    public class LoanTermShould
+    {
+        [Test]
+        public void ReturnTerminMonths()
+        {
+            var sut = new LoanTerm(1);
+
+            Assert.That(sut.ToMonths(), Is.EqualTo(12));
+
+        } 
+    }
+}
